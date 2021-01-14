d8b4d30331dfedf23ff8019864322f02fb729783
erwan-joly <erwan.joly@pushpay.com>
2021-01-14 21:11:12 +1300

    quick improve of setup script

--- scripts/setup-service.cmd
+++ scripts/setup-service.cmd
@@ -1,3 +1,4 @@
+cd %~dp0
 cd ..
 net stop SharesightImporter
 taskkill /F /IM mmc.exe
@@ -6,3 +7,4 @@ dotnet restore
 dotnet publish -c Release -o ./publish/bin/SharesightImporter --runtime win10-x64
 sc.exe create SharesightImporter binpath= %CD%\publish\bin\SharesightImporter\SharesightImporter.exe start=auto
 net start SharesightImporter
+PAUSE
