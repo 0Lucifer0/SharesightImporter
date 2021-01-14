1e45156ce672a58d6763f44ac0913b30fa6c0ca6
erwan-joly <erwan.joly@pushpay.com>
2021-01-14 19:30:13 +1300

    fix install script

--- scripts/setup-service.cmd
+++ scripts/setup-service.cmd
@@ -3,6 +3,6 @@ net stop SharesightImporter
 taskkill /F /IM mmc.exe
 sc.exe delete SharesightImporter
 dotnet restore
-dotnet publish -c Release -o ./publish/bin/SharesightImporter
+dotnet publish -c Release -o ./publish/bin/SharesightImporter --runtime win10-x64
 sc.exe create SharesightImporter binpath= %CD%\publish\bin\SharesightImporter\SharesightImporter.exe start=auto
 net start SharesightImporter
